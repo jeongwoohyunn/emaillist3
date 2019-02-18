ffe1f03a7055de42c16758fb952cf54bd4936e25
bit <bit@DESKTOP-74J9BGV>
2019-02-18 22:56:18 +0900

    주석 및 보충하기 spring활용 guestbook

--- src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -1,15 +1,45 @@
 package com.douzone.emaillist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
 
-	@ResponseBody
-	@RequestMapping("/index")
-	public String index() {
-		return "index";
+	@Autowired //
+	private EmaillistDao emaillistDao;
+
+	/*
+	 * @ResponseBody
+	 * 
+	 * @RequestMapping("") public ModelAndView list() { ModelAndView mav = new
+	 * ModelAndView(); mav.addObject("list", emaillistDao.getList());
+	 * mav.setViewName("/WEB-INF/views/list.jsp");
+	 * 
+	 * return mav;
+	 */
+
+	@RequestMapping("")
+	public String list(Model model) {
+		model.addAttribute("list", emaillistDao.getList());
+		return "/WEB-INF/views/list.jsp";
+	}
+
+	@RequestMapping("/form")
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(EmaillistVo emaillistVo) {
+		emaillistDao.insert(emaillistVo);
+
+		return "redirect:/";
 	}
 }
